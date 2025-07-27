c57b5aa2ad6f35f628d5ac9e42014b540aaea7a3
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-07-27 22:39:29 +0600

    crud cliet recap

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+
+function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    if (!name || !email) {
+      alert("Please fill in all fields");
+      return;
+    }
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+
+    form.reset();
+  };
+  return (
+    <>
+      <h1>Simple CRUD Operations</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
+  );
+}
+
+export default App;
