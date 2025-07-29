ff0b816d4805f433cc9df2eba49b6e7ba2efe304
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-07-29 23:52:20 +0600

    delete

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 export default function User() {
-  const user = useLoaderData();
+  const loadedUser = useLoaderData();
+  const [user, setUser] = useState(loadedUser);
+  const handleDelete = (_id) => {
+    console.log("Delete: ", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("User deleted successfully");
+          const remainingUsers = user.filter((user) => user._id !== _id);
+          setUser(remainingUsers);
+        }
+      });
+  };
   return (
     <div>
       <h2>Total Users: {user.length}</h2>
@@ -11,6 +27,7 @@ export default function User() {
           <p key={user._id}>
             {" "}
             {user.name} - {user.email}
+            <button onClick={() => handleDelete(user._id)}>X</button>
           </p>
         ))}
       </div>
